7609cb812e27f9e74063512980bd0c2422c3d320
Cho Koung Min <andy123423@naver.com>
2024-06-12 16:19:13 +0900

    feat: Aop 로깅기능 구현

--- Areadevelopment/src/main/java/com/sparta/areadevelopment/AreadevelopmentApplication.java
+++ Areadevelopment/src/main/java/com/sparta/areadevelopment/AreadevelopmentApplication.java
@@ -2,13 +2,12 @@ package com.sparta.areadevelopment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableJpaAuditing
 @SpringBootApplication
-
+@EnableAspectJAutoProxy
 public class AreadevelopmentApplication {
 
     public static void main(String[] args) {
