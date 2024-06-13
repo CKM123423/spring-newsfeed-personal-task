d1eae856fbba37d6e8bd1eb819d6beec6457a1c1
Cho Koung Min <andy123423@naver.com>
2024-06-13 16:57:29 +0900

    test: BoardRequestDto 테스트코드 작성

---
+++ Areadevelopment/src/test/java/com/sparta/areadevelopment/dto/BoardRequestDtoTest.java
@@ -0,0 +1,98 @@
+package com.sparta.areadevelopment.dto;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BoardRequestDtoTest {
+
+    private static ValidatorFactory factory;
+    private static Validator validator;
+    private static ObjectMapper objectMapper;
+
+    private BoardRequestDto requestDto;
+
+    @BeforeAll
+    @DisplayName("검증 필요 객체 주입")
+    static void setUp() {
+        factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    @DisplayName("Dto - 생성 성공 테스트")
+    void test1() throws Exception {
+        // Given
+        String json = "{\"title\":\"Board Title\", \"content\":\"Board Content\"}";
+
+        // When
+        requestDto = objectMapper.readValue(json, BoardRequestDto.class);
+
+        // Then
+        Set<ConstraintViolation<BoardRequestDto>> violations = validator.validate(requestDto);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Dto - 제녹 누락 테스트")
+    void test2() throws Exception {
+        // Given
+        String json = "{\"title\":\"\", \"content\":\"Board Content\"}";
+
+        // When
+        requestDto = objectMapper.readValue(json, BoardRequestDto.class);
+
+        // Then
+        Set<ConstraintViolation<BoardRequestDto>> violations = validator.validate(requestDto);
+        assertFalse(violations.isEmpty());
+        assertThat(violations.size()).isEqualTo(1);
+        assertThat(violations.iterator().next().getMessage()).isEqualTo("제목을 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("Dto - 본문 누락 테스트")
+    void test3() throws Exception {
+        // Given
+        String json = "{\"title\":\"Board Title\", \"content\":\"\"}";
+
+        // When
+        requestDto = objectMapper.readValue(json, BoardRequestDto.class);
+
+        // Then
+        Set<ConstraintViolation<BoardRequestDto>> violations = validator.validate(requestDto);
+        assertFalse(violations.isEmpty());
+        assertThat(violations.size()).isEqualTo(1);
+        assertThat(violations.iterator().next().getMessage()).isEqualTo("내용을 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("Dto - 전부 누락 테스트")
+    void test4() throws Exception {
+        // Given
+        String json = "{\"title\":\"\", \"content\":\"\"}";
+
+        // When
+        requestDto = objectMapper.readValue(json, BoardRequestDto.class);
+
+        // Then
+        Set<ConstraintViolation<BoardRequestDto>> violations = validator.validate(requestDto);
+        assertFalse(violations.isEmpty());
+        assertThat(violations.size()).isEqualTo(2);
+
+        assertTrue(violations.stream()
+                .anyMatch(violation -> violation.getMessage().equals("제목을 입력해주세요.")));
+        assertTrue(violations.stream()
+                .anyMatch(violation -> violation.getMessage().equals("내용을 입력해주세요.")));
+    }
+}
